9b287778569f8a83155b684a0475b16944903bfa
Jochelo <jochelo_mbq23@hotmail.com>
2020-01-08 13:13:17 -0400

    eliminacion de postulantes

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -22,6 +22,7 @@
                                     <th scope="col">Celular</th>
                                     <th scope="col">Grado Academico</th>
                                     <th scope="col">Titulo Academico</th>
+                                    <th scope="col">Acciones</th>
                                 </tr>
                                 </thead>
                                 <tbody>
@@ -38,27 +39,27 @@
                                         <td>{{$user->telefono_celular}}</td>
                                         <td>{{$user->academico_grado}}</td>
                                         <td>{{$user->academico_titulo}}</td>
-                                    <!--<td>
-                                            <div class="btn-group">
-                                                <a class="btn btn-border-none btn-sm btn-outline-success "
-                                                   href="{{ url('users/'.$user->user_id.'/edit') }}"
-                                                   data-toggle="tooltip"
-                                                   data-placement="top" title="Editar">
-                                                    <span class="fa fa-pencil-alt"></span>
-                                                </a>
-                                                <form class="float-right"
-                                                      action="{{ route('users.destroy',$user->user_id)}}" method="POST"
+                                    <td>
+                                        <div class="btn-group">
+                                            <!--<a class="btn btn-border-none btn-sm btn-outline-success "
+                                               href="{{ url('users/'.$user->user_id.'/edit') }}"
+                                               data-toggle="tooltip"
+                                               data-placement="top" title="Editar">
+                                                <span class="fa fa-pencil-alt"></span>
+                                            </a>-->
+                                            <form class="float-right"
+                                                  action="{{ route('users.destroy',$user->user_id)}}" method="POST"
                                                       onclick="return confirm('Estas seguro de que desea eliminar?')">
                                                     {{ csrf_field() }}
-                                        <input type="hidden" name="_method" value="DELETE">
-                                        <button type="submit"
-                                                class="btn btn-border-none btn-sm btn-outline-danger">
-                                            <span class="fa fa-trash"></span>
-                                        </button>
-                                    </form>
-                                </div>
-                            </td>-->
-                                    </tr>
+                                                <input type="hidden" name="_method" value="DELETE">
+                                                <button type="submit"
+                                                    class="btn btn-border-none btn-sm btn-outline-danger">
+                                                <span class="fa fa-trash"></span>
+                                                </button>
+                                            </form>
+                                        </div>
+                                    </td>
+                                </tr>
                                 @endforeach
                                 </tbody>
                             </table>
