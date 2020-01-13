92ba498181f09fe00a54236714b4df8d8726995c
Jochelo <jochelo_mbq23@hotmail.com>
2020-01-13 13:34:03 -0400

    reportes test aprobado update

--- resources/views/reporte/test-aprobado.blade.php
+++ resources/views/reporte/test-aprobado.blade.php
@@ -3,12 +3,14 @@
     Fpdf::SetFont('Courier', 'B', 18);
 
     Fpdf::Image('./images/logo.png',20,10,20,20);
+    Fpdf::SetMargins(10, 70, 0);
+    Fpdf::cell(0, 15, "", 0, 0, 'C', false);
 
     Fpdf::ln(5);
 
     Fpdf::SetFont('Arial', 'B', 20);
     Fpdf::SetMargins(15, 50, 0);
-    Fpdf::cell(0, 20, utf8_decode(""), 0, 1, 'C', false);
+    Fpdf::cell(0, 20, utf8_decode("EVALUACIÓN"), 0, 1, 'C', false);
     // datos del cliente
 
     Fpdf::SetFont('Arial', '', 16);
@@ -36,16 +38,44 @@
     Fpdf::cell(145, 8, $user->numero_carnet, 0, 1, 'C', true);
 
     Fpdf::ln(5);
+    $count=1;
+    $respuesta_user={
+        {
+            'pregunta': 'quien eres?',
+            'respueta': 'soy yo',
+            'correcto':1
+        },
+        {
+            'pregunta': 'quien eres?',
+            'respueta': 'soy yo',
+            'correcto':1
+        },
+        {
+            'pregunta': 'quien eres?',
+            'respueta': 'soy yo',
+            'correcto':0
+        },
+    };
+    Fpdf::SetFont('Arial', '', 16);
+    Fpdf::cell(5, 8, "", 0, 0, 'C', false);
+    Fpdf::cell(100, 8, "Datos de la Evaluación:", 0, 0, 'L', false);
+
+    Fpdf::SetFont('Arial', '', 10);
     foreach ($respuestas_user as $respuesta_user){
         Fpdf::ln(2);
-    Fpdf::cell(5, 8, "", 0, 0, 'C', false);
-    Fpdf::cell(35, 8, "Carnet Identidad", 0, 0, 'L', false);
-    Fpdf::SetFillColor(225,221,124);
-    Fpdf::cell(145, 8, $user->numero_carnet, 0, 1, 'C', true);
         Fpdf::cell(5, 8, "", 0, 0, 'C', false);
-        Fpdf::cell(35, 8, "Carnet Identidad", 0, 0, 'L', false);
-        Fpdf::SetFillColor(225,221,124);
-        Fpdf::cell(145, 8, $user->numero_carnet, 0, 1, 'C', true);
+        Fpdf::multicell(35, 8, $count .". ".$respuesta_user->pregunta, 0, 'L', 0);
+
+        if($respuestas_user->correcto){
+            Fpdf::cell(10, 8, "", 0, 0, 'C', false);
+            Fpdf::SetFillColor(91,255,89);
+            Fpdf::multicell(40, 8, $respuesta_user->respuesta, 0, 'L', 0);
+        }
+        else{
+            Fpdf::cell(10, 8, "", 0, 0, 'C', false);
+            Fpdf::SetFillColor(255,130,92);
+            Fpdf::multicell(40, 8, $respuesta_user->respuesta, 0, 'L', 0);
+        }
     }
 
     Fpdf::SetFont('Arial', 'I', 12);
